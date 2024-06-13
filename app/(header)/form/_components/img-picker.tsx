59988d23fd49bd0ec3cf7f37a415996577c1e649
Moon Swing <99680362+publicPark@users.noreply.github.com>
2024-06-13 14:23:18 +0000

    꾸준히 합시다

--- app/(header)/form/_components/img-picker.tsx
+++ app/(header)/form/_components/img-picker.tsx
@@ -1,8 +1,10 @@
 'use client';
-import { useRef } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 import style from './img-picker.module.scss';
+import Image from 'next/image';
 
 export default function ImagePicker({ label = '', name = 'image' }) {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
   const imageInput = useRef<HTMLInputElement>(null);
 
   function handlePickClick() {
@@ -11,11 +13,39 @@ export default function ImagePicker({ label = '', name = 'image' }) {
     }
   }
 
+  function handleImageChange(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      if (!file) return;
+
+      const fileReader = new FileReader();
+
+      fileReader.onload = () => {
+        if (typeof fileReader.result === 'string') {
+          setPickedImage(fileReader.result);
+        }
+      };
+      fileReader.readAsDataURL(file);
+    }
+    return;
+  }
+
   return (
     <>
       <div className={style.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={style.controls}>
+          <div className={style.preview}>
+            {!pickedImage && <p>No image picked yet.</p>}
+            {pickedImage && (
+              <Image
+                src={pickedImage}
+                alt="The image selected by the user"
+                style={{ objectFit: 'cover' }}
+                fill
+              />
+            )}
+          </div>
           <input
             className="hidden"
             type="file"
@@ -23,6 +53,7 @@ export default function ImagePicker({ label = '', name = 'image' }) {
             accept="image/*"
             name={name}
             ref={imageInput}
+            onChange={handleImageChange}
           />
           <button type="button" onClick={handlePickClick}>
             Pick an Image
